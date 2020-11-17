28fd9b1405fe3a08bd3dbe675f1e6f72b539feef
Casey <41272778+GitCaseyHub@users.noreply.github.com>
2020-11-17 16:26:23 -0700

    Update Index.java
    Getters and setters for declared variables

--- Index.java
+++ Index.java
@@ -6,4 +6,20 @@ public class Index {
         this.location = location;
         this.taken=taken;
     }
+
+    public int[] getLocation() {
+        return location;
+    }
+
+    public void setLocation(int[] location) {
+        this.location = location;
+    }
+
+    public boolean isTaken() {
+        return taken;
+    }
+
+    public void setTaken(boolean taken) {
+        this.taken = taken;
+    }
 }
