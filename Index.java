c3f97153e39c48617d41309c50ef40de7ddb0749
Casey <41272778+GitCaseyHub@users.noreply.github.com>
2020-11-17 13:54:19 -0700

    Add files via upload
    An Index class for checking positional-adjacent tiles

---
+++ Index.java
@@ -0,0 +1,9 @@
+public class Index {
+    int[] location;
+    boolean taken;
+
+    public Index(int[] location,boolean taken){
+        this.location = location;
+        this.taken=taken;
+    }
+}
