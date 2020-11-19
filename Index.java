6cd16ea5783839a97213241a349a281f04d44424
Casey <41272778+GitCaseyHub@users.noreply.github.com>
2020-11-19 12:17:31 -0700

    Update Index.java
    Overrode the .toString() method for better representation of what each index was; as well, added a connection variable/methods to class for ways of relating indexes to one another if they are connected by roads.

--- Index.java
+++ Index.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+
 public class Index {
     int[] location;
     boolean taken;
+    ArrayList<Index> connections = new ArrayList<Index>();
 
-    public Index(int[] location,boolean taken){
+    public Index(int[] location,boolean taken,ArrayList<Index> connections){
         this.location = location;
         this.taken=taken;
+        this.connections=connections;
     }
 
     public int[] getLocation() {
@@ -21,5 +25,27 @@ public class Index {
 
     public void setTaken(boolean taken) {
         this.taken = taken;
+    }
+
+    public void addConnection(Index secondIndex){
+        this.connections.add(secondIndex);
+        secondIndex.connections.add(this);
+    }
+
+    public boolean isConnectedTo(Index checkedIndex){
+        if(this.connections.contains(checkedIndex) && checkedIndex.connections.contains(this))
+            return true;
+
+        else
+            return false;
+    }
+
+    public ArrayList<Index> getConnections(){
+        return connections;
+    }
+
+    @Override
+    public String toString(){
+        return "(x,y) of Index: ("+this.location[0]+","+this.location[1]+")";
     }
 }
