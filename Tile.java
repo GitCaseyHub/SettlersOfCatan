c05410d68f4c8b942c1470b070cb0de74f279500
Casey <41272778+GitCaseyHub@users.noreply.github.com>
2020-11-19 12:16:14 -0700

    Update Tile.java
    Added vertices of each tile for index comparison

--- Tile.java
+++ Tile.java
@@ -1,10 +1,13 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Tile {
     int[] position;
     String type;
     int num;
     boolean hasRobber;
+    Point upper_left;
+    ArrayList<Point> vertices = new ArrayList<Point>();
 
     public int[] getPosition() {
         return position;
@@ -43,5 +46,18 @@ public class Tile {
         this.type=type;
         this.num=num;
         this.hasRobber=hasRobber;
+        upper_left = new Point(position[0],position[1]);
+        int x=(int)upper_left.getX();
+        int y =(int)upper_left.getY();
+        vertices.add(new Point(x+72,y));
+        vertices.add(new Point(x,y+39));
+        vertices.add(new Point(x,y+116));
+        vertices.add(new Point(x+72,y+155));
+        vertices.add(new Point(x+134,y+39));
+        vertices.add(new Point(x+134,y+116));
+    }
+
+    public ArrayList<Point> getVertices(){
+        return vertices;
     }
 }
