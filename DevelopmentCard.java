526b08ea5df44dd8a604f1cca34e2e0bdc3fe8bf
Casey <41272778+GitCaseyHub@users.noreply.github.com>
2020-11-20 15:57:36 -0700

    Update DevelopmentCard.java
    Added a bit of infrastructure to the Development Card class

--- DevelopmentCard.java
+++ DevelopmentCard.java
@@ -1,4 +1,69 @@
 public class DevelopmentCard{
     String type="";
-    Player receivingPlayer;
+    Player player;
+    CatanBoard cbReference;
+    Player[] otherPlayers;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public CatanBoard getCbReference() {
+        return cbReference;
+    }
+
+    public void setCbReference(CatanBoard cbReference) {
+        this.cbReference = cbReference;
+    }
+
+    public Player[] getOtherPlayers() {
+        return otherPlayers;
+    }
+
+    public void setOtherPlayers(Player[] otherPlayers) {
+        this.otherPlayers = otherPlayers;
+    }
+
+    public DevelopmentCard(String type, Player player, Player[] otherPlayers, CatanBoard cbReference){
+        //Types of Cards: Knight, Victory Points, Road Builder, Year of Plenty, Monopoly
+        this.type=type;
+        this.player=player;
+        this.cbReference=cbReference;
+        this.otherPlayers=otherPlayers;
+        
+        if(type.equals("Knight"))
+            performKnightAction();
+        
+        else if(type.equals("Victory Points"))
+            performVictoryPoints();
+        
+        else if(type.equals("Road Builder"))
+            performRoadBuilding();
+        
+        else if(type.equals("Year of Plenty"))
+            performYearOfPlenty();
+        
+        else
+            performMonopoly();
+    }
+    
+    public void performKnightAction(){}
+    public void performVictoryPoints(){
+        player.changeVictoryPoints(1);
+    }
+    public void performRoadBuilding(){}
+    public void performYearOfPlenty(){}
+    public void performMonopoly(){}
 }
