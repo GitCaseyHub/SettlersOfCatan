217d6c198cf108c36e9c78a44ed24e1bd19ae48b
Casey <41272778+GitCaseyHub@users.noreply.github.com>
2020-11-29 12:46:22 -0700

    Update Index.java
    Got rid of methods pertaining to index connections as doing so via this class caused NullPointerExceptions. Because a second index was required to be connected simultaneously to the first, the program couldn't recognize the second index was instantiated prior. It seemed easier to just use an ArrayList<int[]> instead.

--- Index.java
+++ Index.java
@@ -1,10 +1,13 @@
-import java.util.ArrayList;
-
 public class Index {
     int[] location;
     boolean taken;
-    ArrayList<Index> connections = new ArrayList<Index>();
     int indexID;
+
+    public Index(int[] location, boolean taken, int indexID){
+        this.location = location;
+        this.taken=taken;
+        this.indexID=indexID;
+    }
 
     public int getIndexID() {
         return indexID;
@@ -12,13 +15,6 @@ public class Index {
 
     public void setIndexID(int indexID) {
         this.indexID = indexID;
-    }
-
-    public Index(int[] location, boolean taken, ArrayList<Index> connections, int indexID){
-        this.location = location;
-        this.taken=taken;
-        this.connections=connections;
-        this.indexID=indexID;
     }
 
     public int[] getLocation() {
@@ -35,23 +31,6 @@ public class Index {
 
     public void setTaken(boolean taken) {
         this.taken = taken;
-    }
-
-    public void addConnection(Index secondIndex){
-        this.connections.add(secondIndex);
-        secondIndex.connections.add(this);
-    }
-
-    public boolean isConnectedTo(Index checkedIndex){
-        if(this.connections.contains(checkedIndex) && checkedIndex.connections.contains(this))
-            return true;
-
-        else
-            return false;
-    }
-
-    public ArrayList<Index> getConnections(){
-        return connections;
     }
 
     @Override
