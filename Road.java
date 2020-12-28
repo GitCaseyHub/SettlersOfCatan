73ca686ba160242dce334bfd50c174286d9ed2a6
Casey <41272778+GitCaseyHub@users.noreply.github.com>
2020-12-28 16:19:21 -0700

    Update Road.java
    Expanded road methods and the extent of its usage

--- Road.java
+++ Road.java
@@ -1,11 +1,27 @@
+import java.awt.*;
+
 public class Road {
     int indexA, indexB;
     Player owner;
+    Point position;
+    String roadType;
 
-    public Road(int indexA, int indexB, Player owner){
+    public Point getPosition() {
+        return position;
+    }
+
+    public Road(int indexA, int indexB, Player owner, Point position, String roadType){
         this.indexA=indexA;
         this.indexB=indexB;
         this.owner=owner;
+        this.roadType=roadType;
+        this.position=position;
+    }
+
+    public Road(){}
+
+    public String getRoadType() {
+        return roadType;
     }
 
     public int getIndexA() {
@@ -30,5 +46,14 @@ public class Road {
 
     public void setOwner(Player owner) {
         this.owner = owner;
+    }
+
+    @Override
+    public String toString(){
+        return "Index (A,B): ("+getIndexA()+","+getIndexB()+")";
+    }
+
+    public boolean isConnectedTo(Road road){
+        return road.getIndexA() == this.getIndexA() || road.getIndexA() == this.getIndexB() || road.getIndexB() == this.getIndexA() || road.getIndexB() == this.getIndexB();
     }
 }
