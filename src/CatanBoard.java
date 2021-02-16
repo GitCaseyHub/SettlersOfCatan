d47cbee89365d9fcccebfe933f0e3efc174d386a
Casey <41272778+GitCaseyHub@users.noreply.github.com>
2021-02-15 18:27:18 -0700

    Update CatanBoard.java
    Make sure the showBuiltImage frame for previews always has priority in showing

--- src/CatanBoard.java
+++ src/CatanBoard.java
@@ -217,6 +217,7 @@ public class CatanBoard extends JFrame implements KeyListener,MouseListener {
         buildFrame.setSize(433,312);
         buildFrame.setLocation(309,389);
         buildLabel.addMouseListener(this);
+        buildFrame.setAlwaysOnTop(true);
     }
 
     public void initializeAwardOptionPanes(){
